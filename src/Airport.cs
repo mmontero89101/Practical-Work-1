354c4416f79d18056079e9158079c1a5205e29d6
LuisRollan <9405669@alumnos.ufv.es>
2025-04-23 17:10:11 +0200

    Runway.cs finally in src

---
+++ src/Airport.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace PracticalWork
+{
+    public class Airport
+    {
+        public List<Runway> Runways {get;set;}= new();
+        public List<Aircraft> Aircrafts {get;set;} = new();
+
+        public void AddAircraft()
+        {
+            
+        }
+    }
+}
--- src/Runway.cs
+++ src/Runway.cs
@@ -1,14 +1,34 @@
 using System;
-
 namespace PracticalWork
 {
     public class Runway
     {
-        public string ID {get; set;}
-        public AircraftStatus Status {get; set;}
+        public string ID{get;set;}
+        public string RunwayStatus{get;set;}
+        public Aircraft CurrentAircraft{get;set;}
+        public int TicksAvailability{get;set;}=3;
+        public Runway(string id)
+        {
+            ID=id;
+            RunwayStatus="Free";
+            CurrentAircraft=null;
+        }
+        public bool RequestRunway(Aircraft aircraft){
 
-        public Aircraft CurrentAircraft {get; set;}
-
-        public int TicksAvailability {get; set;} = 3;
+            if(RunwayStatus == "Free"){
+                RunwayStatus ="Occupied";
+                CurrentAircraft = aircraft;
+                return true;
+            }
+            return false;
+        }
+        public void ReleaseRunway()
+        {
+            if(RunwayStatus=="Occupied"&&CurrentAircraft!=null){
+                CurrentAircraft=null;
+                TicksAvailability=3;
+                RunwayStatus="Free";
+            }
+        }
     }
 }
