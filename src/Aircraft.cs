d190a3bced1bc4a31a4762a58fa0c2b1891f0f0f
LuisRollan <9405669@alumnos.ufv.es>
2025-04-27 18:06:26 +0200

    UpdateOfPositionAndFuel Method completed in class Aircraft

--- src/Aircraft.cs
+++ src/Aircraft.cs
@@ -4,15 +4,31 @@ namespace PracticalWork
 {
     public abstract class Aircraft
     {
-        public string ID { get; set; }
-        public string AircraftStatus Status {get; set;}
-        public int Distance { get; set; }
-        public int Speed { get; set; }
-        public double FuelCapacity { get; set; }
-        public double FuelConsumption { get; set; }
-        public double CurrentFuel { get; set; }
 
-        public abstract void UpdateOfPositionAndFuel();
+        public enum AircraftStauts{InFlight,Waiting,Landing,Onground}
+        public string ID {get; set;}
+        public string AircraftStatus Status {get; set;}
+        public int Distance {get; set;}
+        public int Speed {get; set;}
+        public double FuelCapacity {get; set;}
+        public double FuelConsumption {get; set;}
+        public double CurrentFuel {get; set;}
+
+        public abstract void UpdateOfPositionAndFuel()
+        {
+            if(AircraftStatus == AircraftStatus.InFlight)
+            {
+                int distanceToTravel = Speed / 4;
+                int actualTravel = Math.Min(distanceToTravel, Distance);
+                Distance -= actualTravel;
+                CurrentFuel -= actualTravel * FuelConsumption
+                if(Distance <= 0)
+                {
+                    Distance = 0;
+                    Status = AircraftStatus.Waiting;
+                }
+            }
+        }
         
             
         
